36df75b7c2c2f5b9a86e3e10fe3972602085b9b1
Mohamedjasim1 <142711041+Mohamedjasim1@users.noreply.github.com>
2025-12-01 11:10:22 +0530

    Time: 101 ms (15.16%), Space: 75.4 MB (13.61%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -14,24 +14,28 @@ class Solution {
         target/=2;
         dp=new Boolean[nums.length+1][target+1];
 
-        return dfs(0,target,nums);
+        return dfs(0,0,nums,target);
         
     }
 
-    public boolean dfs(int i,int tar,int[] nums){
+    public boolean dfs(int i,int tar,int[] nums,int target){
 
 
-        if(tar==0)return true;
+        if(tar==target)return true;
 
-        if(tar<0)return false;
+
+
+        if(tar>target || i>nums.length-1)return false;
 
         if(dp[i][tar]!=null)return dp[i][tar];
 
-       for(int j=i;j<nums.length;j++){
-            if(dfs(j+1,tar-nums[j],nums))return dp[i][tar]=true;
-       }
+        boolean take=dfs(i+1,tar+nums[i],nums,target);
 
-        return dp[i][tar]=false;
+        boolean skip=dfs(i+1,tar,nums,target);
+
+        
+
+        return dp[i][tar]=take || skip;
 
     }
 }
