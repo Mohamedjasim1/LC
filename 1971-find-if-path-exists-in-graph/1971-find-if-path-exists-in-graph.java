36a4f22ef862b94946dd5f3bbd73ec15c47701bd
Mohamedjasim1 <142711041+Mohamedjasim1@users.noreply.github.com>
2025-12-02 18:31:02 +0530

    Time: 68 ms (77.59%), Space: 265.3 MB (42.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,37 +1,45 @@
 class Solution {
-    ArrayList<ArrayList<Integer>> arr;
+    ArrayList<ArrayList<Integer>> graph;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         
 
-        arr=new ArrayList<>();
+        graph=new ArrayList<>();
         for(int i=0;i<n;i++){
-            arr.add(new ArrayList());
+            graph.add(new ArrayList());
         }
 
         for(int[] e:edges){
-            arr.get(e[0]).add(e[1]);
-            arr.get(e[1]).add(e[0]);
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
 
-        return dfs(source,destination,new boolean[n+1]);
+        return bfs(source,destination,new boolean[n+1]);
 
     }
 
-    public boolean dfs(int s,int d,boolean[] vis){
+    public boolean bfs(int s,int d,boolean[] vis){
 
-        if(s==d)return true;
+        Queue<Integer> q=new LinkedList<>();
 
-        if(vis[s])return false;
-        vis[s]=true;
+        q.add(s);
+        
 
-        for(int n:arr.get(s)){
-            if(vis[n])continue;
-            if(dfs(n,d,vis)){
-                return true;
+        while(!q.isEmpty()){
+
+            
+
+            int curNode=q.poll();
+            if(vis[curNode])continue;
+            vis[curNode]=true;
+
+            if(d==curNode)return true;
+
+            for(int node:graph.get(curNode)){
+                q.add(node);
             }
+
         }
 
         return false;
-
     }
 }
